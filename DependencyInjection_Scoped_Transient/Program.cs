7fb46e37b2280452b9e050f7ddf5120f5272da75
anand.neema14@gmail.com <anand.neema14@gmail.com>
2024-06-20 20:55:09 +0530

    Dependency Injection: Socoped and Transient example

---
+++ DependencyInjection_Scoped_Transient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+Console.Clear();
+var collection=new ServiceCollection();
+collection.AddScoped<Scoped>();
+collection.AddTransient<Transient>();
+
+var provider = collection.BuildServiceProvider();
+Parallel.For(1, 10, i =>
+{
+    var scopedObject = provider.GetService<Scoped>();
+    var transientObject = provider.GetService<Transient>();
+
+    Console.WriteLine($"ScopeId:" + scopedObject.GetHashCode());
+    Console.WriteLine($"TransientId:" + transientObject.GetHashCode());
+});
+
+Console.Write("Press Key");
+Console.ReadKey();
+
+public class Scoped
+{
+
+}
+
+public class Transient
+{
+
+}
