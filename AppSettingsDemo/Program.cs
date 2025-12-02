673468e23d5aed17743fa9e13ecdf850934d8aa6
Anand Neema <anand.neema14@gmail.com>
2025-12-02 17:22:09 +0530

    proj file

---
+++ AppSettingsDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("appSettings.json")
+    .Build();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The Config data from appSettings is:" + configuration["Connection"]);
+Console.ReadLine();
+
+
